2213ccc23b1511d1f815978833ed00a95b1eb8d1
jiee <102215250+jieee3@users.noreply.github.com>
2025-03-26 18:46:18 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -66,10 +66,10 @@ min_hour, max_hour = st.sidebar.slider("Pilih Rentang Jam", 0, 23, (0, 23))
 filtered_hour_bike_rentals = hour_bike_rentals.loc[min_hour:max_hour]
 
 # **Visualisasi Total Peminjaman Sepeda Berdasarkan Jam dalam Sehari**
-st.subheader(f"Total Peminjaman Sepeda Berdasarkan Jam ({min_hour}:00 - {max_hour}:00)")
+st.subheader(f"Total Peminjaman Sepeda Berdasarkan Jam dalam sehari ({min_hour}:00 - {max_hour}:00)")
 fig, ax = plt.subplots(figsize=(10, 6))
 sns.barplot(x=filtered_hour_bike_rentals.index, y=filtered_hour_bike_rentals.values, palette="Purples", ax=ax)
-ax.set_title(f"Total Peminjaman Sepeda Berdasarkan Jam ({min_hour}:00 - {max_hour}:00)")
+ax.set_title(f"Total Peminjaman Sepeda Berdasarkan Jam dalam sehari")
 ax.set_xlabel("Jam dalam Sehari")
 ax.set_ylabel("Total Peminjaman Sepeda")
 ax.set_ylim(0, hour_bike_rentals.max() * 1.1)  # Menyesuaikan skala sumbu Y
